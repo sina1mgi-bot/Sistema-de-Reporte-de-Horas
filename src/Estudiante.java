cfad2e429c9b66c3d6e88166c48441a726863c26
lpiedras4 <piedras.leo@gmail.com>
2025-11-29 23:03:14 -0800

    Se agregaron metodos faltantes de los roles

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -6,6 +6,7 @@ public class Estudiante extends Usuario {
     private int totalHoras;
     private Proyecto proyectoAsignado;
     private List<ReporteDeHoras> reportes;
+    private String nombre;
 
 
     public Estudiante(String nombre, String psww, String matricula) {
@@ -13,6 +14,34 @@ public class Estudiante extends Usuario {
         this.matricula = matricula;
         this.totalHoras = 0;
         this.reportes = new ArrayList<>();
+        this.nombre = nombre;
+    }
+
+    public List<Proyecto> consultarProyectos(List <Proyecto> disponibles) {
+        return disponibles;
+    }
+
+    public void asignarseProyecto (Proyecto proyecto) {
+        this.proyectoAsignado = proyecto;
+        proyecto.agregarEstudiante(this);
+    }
+    public void getNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public ReporteDeHoras crearReporte(String descripcion, int horas){
+        ReporteDeHoras reporte = new ReporteDeHoras(descripcion, this, horas);
+        reportes.add(reporte);
+        return reporte;
+    }
+
+    public void corregirReporte (ReporteDeHoras reporte, String nuevaDescripcion, int nuevasHoras){
+        reporte.setDescripcion(nuevaDescripcion);
+        reporte.actualizarHoras(nuevasHoras);
+    }
+
+    public void sumarHoras(int horas) {
+        if (horas > 0) {this.totalHoras += horas;}
     }
 
 }
