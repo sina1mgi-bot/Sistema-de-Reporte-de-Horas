3d47219f7d700e1a2ed10beec2c3562235f6a2a2
lpiedras4 <piedras.leo@gmail.com>
2025-11-30 00:30:53 -0800

    cambios en master

--- src/VentanaInicioSesion.java
+++ src/VentanaInicioSesion.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
-public class VentanaInicioSesion {
+public class VentanaInicioSesion extends JFrame{
+    private JPanel loginPanel;
+    private JLabel InicioSesionLbl;
+    private JLabel tituloBienve;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
 }
